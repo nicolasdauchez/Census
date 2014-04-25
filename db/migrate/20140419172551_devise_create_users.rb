b5c6f8b784b569d03da15b7fd06ccdba91f4af2c
lumy <lumy@lumy.me>
2014-04-25 14:46:41 -0400

    try patch

--- db/migrate/20140419172551_devise_create_users.rb
+++ db/migrate/20140419172551_devise_create_users.rb
@@ -4,10 +4,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :name, null: false, default: ""
-      t.integer :status
-      t.primary_key :regisration
-
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -37,7 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :users, :email,:regisration, unique: true
+    add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
