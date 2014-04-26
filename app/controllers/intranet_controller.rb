214ab42ad38e6231e6d5dedf6fb16b1b6485f376
nicolasdauchez <nicolas.dauchez@epitech.eu>
2014-04-26 14:00:21 +0100

    some minor changes on index

--- app/controllers/intranet_controller.rb
+++ app/controllers/intranet_controller.rb
@@ -1,16 +1,6 @@
 class IntranetController < ApplicationController
-   before_action :authenticate_user!
-
+  before_action :authenticate_user!
   def index
-    if current_user.status == 0
-      ## Student Account
-      return "Student"
-    elsif current_user.status == 1
-      ## Lecturer Account
-      return "Prof"
-    elsif current_user.status == 2
-      ## Admin Account
-      return "Admin"
-    end
+    @welcomeStr = "BLAH"
   end
 end
